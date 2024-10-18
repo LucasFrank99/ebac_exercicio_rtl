b7a7d7fc3cef4c0576be542fc66c54a5524b673b
lucas Frank <lucasfrank11@hotmail.com>
2024-10-18 16:12:43 -0300

    feito

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+
 import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Deve inserir um comentario', () => {
+        render(<App/>)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value:'novo comentario'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        expect(screen.getByText('novo comentario')).toBeInTheDocument()
+    })
+
+    test('Deve inserir outro comentario', () => {
+        render(<App/>)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value:'novo comentario'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        expect(screen.getByText('novo comentario')).toBeInTheDocument()
+    })
 });
